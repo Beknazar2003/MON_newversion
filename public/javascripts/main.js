10fd28b269679ff49c5a3cf40ff0d6b9123cfc75
Beknazar2003 <bektashev2003@mail.ru>
2021-03-26 10:14:30 +0600

    fetch

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,18 +1,46 @@
+//GET data
+const requestURL = 'http://localhost:3000/data'
+let GETdata
+function sendRequest(method, url) {
+  const headers = {
+    'Content-Type': 'application/json'
+  }
+
+  return fetch(url, {
+    method: method,
+    headers: headers
+  }).then(response => {
+    if (response.ok) {
+      return response.json()
+    }
+
+    return response.json().then(error => {
+      const e = new Error('Что-то пошло не так')
+      e.data = error
+      throw e
+    })
+  })
+}
+
+sendRequest('GET', requestURL)
+  .then(data => {GETdata = data})
+  .catch(err => console.log(err))
+
 google.load("visualization", "1", {packages:["corechart"]});
 google.setOnLoadCallback(drawChart);
 
-function drawChart() {
-    const data = google.visualization.arrayToDataTable([
+async function drawChart() {
+    const data =await google.visualization.arrayToDataTable([
      ['Time', 'Total'],
-     ['2018-2019', 95],
-     ['2019-2020', 95],
-     ['2020-2021', 98]
+     ['2018-2019', GETdata[0].value],
+     ['2019-2020', GETdata[1].value],
+     ['2020-2021', GETdata[2].value]
     ]);
     const options = {
      colors: ['brown']
     };
     const chart = new google.visualization.ColumnChart(document.getElementById('histogram-1'));
-    const chart2 = new google.visualization.ColumnChart(document.getElementById('histogram-2'));
+    const chart2 = new google.visualization.PieChart(document.getElementById('histogram-2'));
     chart.draw(data, options);
     chart2.draw(data, options);
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Дошкольное образование' });
-});
+  res.render('index', { 
+    title: 'Дошкольное образование',
+   })
+})
+
+router.get('/data', (req, res) => {
+  const data = [
+    {value: 95},
+    {value: 95},
+    {value: 98}
+  ]
+  res.send(data)
+})
 
 module.exports = router;
