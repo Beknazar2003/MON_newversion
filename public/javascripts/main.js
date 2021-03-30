f66bf8fdea33afb16c0b370824a110fac986009b
Beknazar2003 <bektashev2003@mail.ru>
2021-03-30 15:10:53 +0600

    jQuery connect

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,49 +1,7 @@
-//GET data
-const requestURL = 'http://localhost:3000/data'
-
-let GETdata
-
-function sendRequest(method, url) {
-  const headers = {
-    'Content-Type': 'application/json',
-  }
-
-  return fetch(url, {
-    method: method,
-    headers: headers
-  }).then(response => {
-    if (response.ok) {
-      return response.json()
-    }
-
-    return response.json().then(error => {
-      const e = new Error('Что-то пошло не так')
-      e.data = error
-      throw e
-    })
+$('document').ready(() => {
+  
+  $('select#TimeFilter').change((options) => {
+        console.log(options.currentTarget.value)
   })
-}
-
-sendRequest('GET', requestURL)
-  .then(data => {
-    GETdata = data
-    google.charts.load('49');
-    google.charts.setOnLoadCallback(drawVisualization);
-
-      function drawVisualization() {
-        GETdata.forEach(element => {
-          const wrapper = new google.visualization.ChartWrapper(element);
-          const para = document.createElement("div");
-          para.setAttribute("id", element.containerId)
-          para.setAttribute("class", "card")
-          
-          const elem = document.getElementById("kg_std_doo");
-          elem.appendChild(para);
-          wrapper.draw();
-        });
-        
-
-        
-      }
-  })
-  .catch(err => console.log(err))
+  
+})
