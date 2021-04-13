7e7639ec3a452ab7d6e7d3bf2b818f8fa5698c09
Beknazar2003 <bektashev2003@mail.ru>
2021-04-13 16:02:31 +0600

    fix

--- public/libs/sendRequest.js
+++ public/libs/sendRequest.js
@@ -1,34 +1,34 @@
-// const GETdata = new Array()
-// const requestURL = '/data/data'
+const GETdata = new Array()
+const requestURL = '/data/data'
 
-// google.charts.load('49', {
-//   'packages': ['corechart', 'controls', 'bar']
-// })
+google.charts.load('49', {
+    'packages': ['corechart', 'controls', 'bar']
+})
 
-// function sendRequest(method, url) {
-//   const headers = {
-//     'Content-Type': 'application/json',
-//   }
+function sendRequest(method, url) {
+    const headers = {
+        'Content-Type': 'application/json',
+    }
 
-//   return fetch(url, {
-//     method: method,
-//     headers: headers
-//   }).then(response => {
-//     if (response.ok) {
-//       return response.json()
-//     }
+    return fetch(url, {
+        method: method,
+        headers: headers
+    }).then(response => {
+        if (response.ok) {
+            return response.json()
+        }
 
-//     return response.json().then(error => {
-//       const e = new Error('Что-то пошло не так')
-//       e.data = error
-//       throw e
-//     })
-//   })
-// }
+        return response.json().then(error => {
+            const e = new Error('Что-то пошло не так')
+            e.data = error
+            throw e
+        })
+    })
+}
 
-// sendRequest('GET', requestURL)
-//   .then(data => {
-//     data.forEach(element => {
-//       GETdata.push(element)
-//     })
-//   })
+sendRequest('GET', requestURL)
+    .then(data => {
+        data.forEach(element => {
+            GETdata.push(element)
+        })
+    })
